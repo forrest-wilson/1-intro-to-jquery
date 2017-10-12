b29a2b1cb307703895dca8e334d2056c1d6060cb
Forrest Wilson <forrest.wilson@me.com>
2017-10-13 11:26:47 +1300

    Added 4 boxes that do different things with their buttons

--- js/app.js
+++ js/app.js
@@ -2,4 +2,30 @@ $(document).ready(function() {
     $("#clickableButton").click(function() {
         $("#theId").toggle(500);
     });
+
+    $("#box1 button").click(function() {
+        $("#box1").css({
+            "background-color": "red"
+        });
+    });
+
+    $("#box2 button").click(function() {
+        $("#box2").toggleClass("add-border");
+    });
+
+    $("#box3 button").click(function() {
+        $("#box3").innerHeight(500);
+    });
+
+    $("#box4 button").hover(function() {
+        $(this).css({
+            "height": "100%",
+            "width": "100%",
+            "transform": "none",
+            "left": "0",
+            "top": "0"
+        });
+    }, function() {
+        alert("WHAT'S GOING ON!");
+    });
 });
