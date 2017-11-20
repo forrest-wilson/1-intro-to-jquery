c0f44a7200900f5007f84ced99a7b3fb217ce56f
Forrest Wilson <forrest.wilson@me.com>
2017-11-21 12:47:09 +1300

    Seperated array-methods

---
+++ array-methods/indexOf.js
@@ -0,0 +1,19 @@
+// indexOf() Array Method
+
+var arr = ["Fish", "Tigers", "Sheep", "Tigers", "Horses"],
+    found = false;
+
+for (var i= 0, l = arr.length; i< l; i++){
+    if (arr[i] === 'Tigers') {
+        found = true;
+        console.log('run!');
+    }
+}
+
+// By utilzing -1 in the indexOf syntax we can scan the array
+// and return a true result
+var sayWhat = arr.indexOf("Tigers") != -1;
+console.log(sayWhat);
+
+//Another pattern is using only the value of the item itself
+console.log(arr.indexOf("Tigers", 2));
